7742555234cc51f73f69bcf24a4189c632445a03
v01dssh <v01dssh@proton.me>
2023-10-03 23:07:40 +0300

    Add playRound logic which is the game algorithm that determines the winner

--- app.js
+++ app.js
@@ -1,9 +1,48 @@
 // Take Player Input
 let playerChoice = prompt("Rock, Paper, or Scissors ?").toLowerCase();
 
+let playerWins = 0;
+let computerWins = 0;
+
 // Computer choice logic
 function computerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
     let choices = ["rock", "paper", "scissors"];
     return choices[randomNumber];
 }
+
+function playRound(playerChoice, computerChoice) {
+    // Rock beats Scissors 
+    if (playerChoice === "rock" && computerChoice === "scissors") {
+        playerWins += 1;
+    }
+    else if (playerChoice === "scissors" && computerChoice === "rock") {
+        computerWins += 1;
+    }
+
+    // Paper beats Rock
+    else if (playerChoice === "paper" && computerChoice === "rock") {
+        playerWins += 1;
+    }
+    else if (playerChoice === "rock" && computerChoice === "paper") {
+        computerWins += 1;
+    }
+
+    // Scissors and Paper
+    else if (playerChoice === "scissors" && computerChoice === "paper") {
+        playerWins += 1;
+    }
+    else if (playerChoice === "paper" && computerChoice === "scissors") {
+        computerWins += 1;
+    }
+
+    // Tie 
+    else {
+        console.log("Tie");
+    }
+}
+
+playRound(playerChoice, computerChoice());
+
+console.log(playerWins);
+console.log(computerWins);
